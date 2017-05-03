a3e3d058ad8103cd67b90eeb9a5efbb978b081ba
BruceLEO1969 <BruceLEO1969@gmail.com>
2017-05-03 16:41:59 +0800

    add tmux config

--- non_root.sh
+++ non_root.sh
@@ -16,15 +16,30 @@ privilege_check(){
 
 # zsh config
 zsh_config(){
+  cd ~
   mkdir -p .antigen
   echo "Setting up antigen, just be patients."
   curl -L git.io/antigen > .antigen/antigen.zsh &>/dev/null
   echo "Update .zshrc file."
-  curl -LO 
+  curl -LO https://raw.githubusercontent.com/BruceLEO1969/vps2arch/master/.zshrc &>/dev/null
+  source .zshrc &>/dev/null
+  echo "zsh_config finished."
 }
+
+# tmux_config
+tmux_config(){
+  rm -rf .tmux
+  git clone https://github.com/BruceLEO1969/.tmux.git
+  ln -s -f .tmux/.tmux.conf
+  cp .tmux/.tmux.conf.local .
+  echo "tmux setup finished."
+}
+
 # main function
 main(){
   privilege_check
+  zsh_config
+  tmux_config
 }
 
 main
